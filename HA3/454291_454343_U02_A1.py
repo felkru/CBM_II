a9ab51b2c00d62b72237d06f6bcb6c7dd4ce4cae
Felix Krückel <acc@felkru.com>
2025-04-22 17:36:27 +0200

    added ex3 boilerplate

---
+++ HA3/454291_454343_U02_A1.py
@@ -0,0 +1,17 @@
+#! /usr/bin/env python3
+# -*- coding: utf-8 -*-
+from itertools import count
+
+### Vorlagendatei für die Übungen zur Computergestützten Physik ###
+
+#
+# 1)
+# Matrikelnummer: 454291
+# Name: Julia Els
+# Email: julia.els@rwth-aachen.de
+#
+# 2)
+# Matrikelnummer: 454343
+# Name: Felix Krückel
+# Email: felix.krueckel@rwth-aachen.de
+#
