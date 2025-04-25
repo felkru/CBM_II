39ba41d75a08fc4d7598fd24a81b2971492c3354
Felix Krückel <acc@felkru.com>
2025-04-25 16:55:40 +0200

    added robust input logic for dice roll input

---
+++ HA3/454291_454343_U03_A1.py
@@ -0,0 +1,49 @@
+#! /usr/bin/env python3
+# -*- coding: utf-8 -*-
+from itertools import count
+
+### Vorlagendatei für die Übungen zur Computergestützten Physik ###
+
+#
+# 1)
+# Matrikelnummer: 454291
+# Name: Julia Els
+# Email: julia.els@rwth-aachen.de
+#
+# 2)
+# Matrikelnummer: 454343
+# Name: Felix Krückel
+# Email: felix.krueckel@rwth-aachen.de
+#
+
+import numpy as np
+
+def input_mode():
+    # ask for user input as long they
+    new_rolls = []
+    while True:
+        try:
+            dice_res = int(input("Welche Augenzahl zeigt ihr Würfel?\n").strip())
+        except Exception:
+            print('Eingabe konnte nicht geparst werden. Geben sie eine Zahl ein.')
+            continue
+        new_rolls.append(dice_res)
+        add_more = input('Do you want to add additional dice rolls? (y/n): ').lower().strip() == 'y'
+        if (not add_more):
+            np.savetxt("My_Dice_Rolls.csv", [*rolls, *new_rolls])
+            return
+
+# @Tutor - Program execution starts here
+try:
+    rolls = np.loadtxt("My_Dice_Rolls.csv", dtype=int, converters=float)
+    add_input = input('Do you want to add additional dice rolls? (y/n): ').lower().strip() == 'y'
+    if (add_input):
+        input_mode()
+except FileNotFoundError:
+    print('Würfelergebnisdatei nicht gefunden. Starte den interaktiven Modus.')
+    rolls = []
+    input_mode()
+except Exception:
+    print('Es ist beim laden der Datei ein Fehler aufgetreten, der sich nicht automatisch korrigieren lässt. Bitte überprüfen sie den Inhalt und Namen ihrer Würfelergebnisdatei.')
+    print('Continuing with example dice_rolls.')
+    rolls = [1,2,5,6,1,3,5,4]
---
+++ HA3/454291_454343_U03_A2.py
@@ -0,0 +1,62 @@
+#! /usr/bin/env python3
+# -*- coding: utf-8 -*-
+from itertools import count
+
+### Vorlagendatei für die Übungen zur Computergestützten Physik ###
+
+#
+# 1)
+# Matrikelnummer: 454291
+# Name: Julia Els
+# Email: julia.els@rwth-aachen.de
+#
+# 2)
+# Matrikelnummer: 454343
+# Name: Felix Krückel
+# Email: felix.krueckel@rwth-aachen.de
+#
+import numpy as np
+import matplotlib.pyplot as plt
+import scipy as scp
+
+def setup_plot(title, y_label='y'):
+    plt.figure(figsize=(10, 6))
+    plt.xlabel('x')
+    plt.ylabel(y_label)
+    plt.title(title)
+    plt.grid(True)
+
+
+def display_plot():
+    plt.legend()
+    plt.show()
+
+
+data = np.genfromtxt('linfit_data_3.csv', delimiter=',', skip_header=1)
+
+x, y, var = data[:, 0], data[:, 1], data[:, 2]
+std = np.sqrt(var)
+
+# %%
+print('--- 2 a) ---')
+setup_plot('Messpunkte mit Fehlerbalken (linfit_data_3.csv)')
+plt.errorbar(x, y, yerr=std, fmt='o', label='Messpunkte mit Fehlerbalken', capsize=3)
+
+print('--- 2 b) ---')
+
+linear_model = lambda x, m, c: m * x + c
+
+params_no_error, cov_no_error  = scp.optimize.curve_fit(linear_model, x, y, sigma=std, absolute_sigma=False)
+params_w_error, cov_w_error = scp.optimize.curve_fit(linear_model, x, y, sigma=std, absolute_sigma=True)
+plt.plot(x, linear_model(x, *params_no_error), label='Fitted linear model w/ and w/o error prop') # hier könnte man auch params_w_error verwenden, da die Werte identisch sind, da sie nur vom Datenset und den relativen Fehlern abhängen.
+print(f'Ohne Fehlerpropagation (absolute_sigma=False) ergit sich für m={params_no_error[0]}±{np.sqrt(cov_no_error[0,0])} und c={params_no_error[1]}±{np.sqrt(cov_no_error[1,1])}')
+print(f'Mit Fehlerpropagation (absolute_sigma=True) ergit sich für m={params_w_error[0]}±{np.sqrt(cov_w_error[0,0])} und c={params_w_error[1]}±{np.sqrt(cov_w_error[1,1])}')
+print('Mit Fehlerpropagation ist der Fehler wesentlich kleiner und wir würden entsprechend unseren Fehler unterschätzen, wenn absolute_error=False.')
+display_plot()
+
+print('--- 2 c) ---')
+y_at_3 = linear_model(x, *params_w_error)
+y_at_3_error = np.norm(params_w_error[0,0], params_w_error[1,1])
+print(f'f(3) = {y_at_3}±{y_at_3_error}, wenn man annimmt, dass m und c unkorreliert sind.')
+
+print('--- 2 d) ---')
